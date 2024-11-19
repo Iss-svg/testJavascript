2975704d0479b25344050b6a9ab15315198fc282
hatibou iss-haka <i.hamadahatibou@gmail.com>
2024-11-19 13:03:09 +0100

    lok

---
+++ script.js
@@ -0,0 +1,34 @@
+//exercise 1
+console.log(5+2)
+console.log(5 + "2")
+console.log(5 * 2)
+console.log(5 * "2" )
+
+
+//Exercise 2
+let I= 0;
+I=I+1;
+I=I+1;
+I=I+1;
+console.log(I)
+
+//Exercise 3
+//définir une variabla nom prénom
+const firstname="iss-haka";
+const lastname="HATIBOU"
+const nomComplet=firstname+lastname;
+//affiche en console nom complet
+console.log(nomComplet);
+
+
+//Exercise 4
+//écrire en console de 2 manière différentes la phrase 
+console.log(`Bonjour`+ firstname +`comment va tu`)
+console.log(`Bonjour ${firstname} comment vas tu ?`) //interpolation
+
+//Exercise 5
+
+const phrase="je m'apelle iss-haka et j'ai faim"
+console.log(phrase.length) 
+
+
